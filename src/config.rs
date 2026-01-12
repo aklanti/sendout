279c1ccd6476e5cab16d49fc23837c0a180fc66f
Sede Soukossi <4968379+issokuos@users.noreply.github.com>
2026-01-12 15:20:04 +0100

    test: test configuration instantiation

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 //! Email sending configuration data
 
-use secrecy::SecretBox;
+use secrecy::SecretString;
 
 /// Configuration for the email sending service
 #[must_use]
@@ -12,11 +12,30 @@ pub struct EmailConfig {
     /// Secret API token for authentication
     ///
     /// This token is used when making API requests
-    pub api_token: SecretBox<String>,
+    pub api_token: SecretString,
 
     /// The verified sender email address
     ///
     /// This email must be a sender verified by your email service provider
     /// Emails will appears to come from this address
     pub from_email: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::EmailConfig;
+    use secrecy::{ExposeSecret, SecretString};
+
+    #[test]
+    fn test_email_config() {
+        let config = EmailConfig {
+            api_token: SecretString::from(String::from("test-token")),
+            from_email: "from@test.com".into(),
+            base_url: "http://localhost:6666".into(),
+        };
+
+        assert_eq!(config.api_token.expose_secret(), "test-token");
+        assert_eq!(config.from_email, "from@test.com");
+        assert_eq!(config.base_url, "http://localhost:6666");
+    }
 }
